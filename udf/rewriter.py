39039a3bce9dc95a05974bf68bbf248b0e26ccce
kai-franz <kfranz@andrew.cmu.edu>
2023-03-20 14:10:27 -0400

    Added Rewriter class

---
+++ udf/rewriter.py
@@ -0,0 +1,19 @@
+from query_rewriter import *
+from udf_rewriter import *
+
+
+class Rewriter:
+    def __init__(self, query, udf):
+        self.query = query
+        self.udf = udf
+
+        # Do the rewrites
+        self.udf_rewriter = UdfRewriter(udf)
+        self.root_q = parse_sql(query)
+        transformQuery(self.root_q, self.udf_rewriter.original_func_name)
+
+    def new_query(self):
+        return IndentedStream()(self.root_q)
+
+    def new_udf(self):
+        return self.udf_rewriter.output()
