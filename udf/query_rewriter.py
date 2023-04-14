035b33646055fc7d3dbaa2a8daf8bb79e3012666
kai-franz <kfranz@andrew.cmu.edu>
2023-04-14 17:27:54 -0400

    Support WHERE clause in calling query

--- udf/query_rewriter.py
+++ udf/query_rewriter.py
@@ -31,7 +31,8 @@ def get_function_calls(parseTree, udf_name):
         function_calls = []
 
         def visit_FuncCall(self, parent, node):
-            if node.funcname[0].val == udf_name:
+            print(node.funcname)
+            if node.funcname[-1].val == udf_name:
                 self.function_calls.append(node)
 
     visitor = FunctionCallAccumulator()
@@ -39,7 +40,23 @@ def get_function_calls(parseTree, udf_name):
     return visitor.function_calls
 
 
-def convert_to_subquery(q: ast.SelectStmt):
+class FunctionCallSplitter(Visitor):
+    """
+    Given an expression like f(x + 2) > 0, we want to split this expression into two
+    parts:
+    1. f(x + 2)
+    2. f > 0
+    """
+
+    def __init__(self, func_name: str):
+        self.func_name = func_name
+
+    def visit_FuncCall(self, parent, node):
+        if node.funcname[-1].val == self.func_name:
+            return ast.ColumnRef(fields=(self.func_name,))
+
+
+def convert_to_subquery(q: ast.SelectStmt) -> ast.SelectStmt:
     """
     Converts a query of the form (SELECT c1, c2, c3 FROM ...) into
     (SELECT c1, c2, c3 FROM (SELECT c1, c2, c3 FROM ...) as dt1)
@@ -116,20 +133,69 @@ def get_unique_col_refs(col_refs: List[ast.ColumnRef]):
     return col_refs
 
 
+def remove_where_clause_udf(select_stmt, udf_name):
+    where_clause = select_stmt.whereClause
+    udf_clause = None
+    if isinstance(where_clause, ast.A_Expr):
+        udf_clause = where_clause
+        select_stmt.whereClause = None
+    elif isinstance(where_clause, ast.BoolExpr):
+        for i, clause in enumerate(where_clause.args):
+            fn_calls = get_function_calls(clause, udf_name)
+            if len(fn_calls) > 0:
+                udf_clause = clause
+                # remove the ith element from where_clause
+                where_clause.args = where_clause.args[:i] + where_clause.args[i + 1 :]
+                break
+    else:
+        raise Exception("Unexpected where clause type")
+    if not udf_clause:
+        raise Exception("UDF not found in where clause")
+    return udf_clause
+
+
 def transform_query(q, udf_name):
     """
     Transforms a query into a batched form
     :param q:
     :return:
     """
+    print(udf_name)
     subquery = q[0].stmt
     q[0].stmt = convert_to_subquery(subquery)
     select_stmt = q[0].stmt
+    fn_calls = []
+    udf_in_where_clause = False
+    udf_clause = None
+    if subquery.whereClause is not None:
+        fn_calls = get_function_calls(subquery.whereClause, udf_name)
+        if len(fn_calls) > 0:
+            assert len(fn_calls) == 1
+            print("UDF in where clause")
+            # UDF in the where clause. We need to move it to the select clause.
+            # Then, at the end, we'll add the where clause back in.
+            udf_in_where_clause = True
+            udf_clause = remove_where_clause_udf(subquery, udf_name)
+            select_stmt.targetList = select_stmt.targetList + (
+                ast.ResTarget(val=fn_calls[0]),
+            )
+            fn_calls[0] = select_stmt.targetList[-1]
+            print(select_stmt.targetList)
+            FunctionCallSplitter(udf_name)(udf_clause)
+            print("udf clause:", udf_clause)
+
+    print(select_stmt.targetList)
     fn_calls = get_function_calls(q, udf_name)
-    inner_targets = []
+    if len(fn_calls) == 0:
+        # No UDFs. Just return.
+        raise Exception("No UDFs found")
+        # return
     assert len(fn_calls) == 1
+
+    inner_targets = []
     fn_call = fn_calls[0]
     fn_call_args = fn_call.args
+    print(select_stmt)
 
     # for target in select_stmt.targetList:
     #     if not (
@@ -147,17 +213,19 @@ def transform_query(q, udf_name):
         target
         for target in copy.deepcopy(unnest_target_list)
         if not (
-            isinstance(target.val, ast.FuncCall)
-            and target.val.funcname[0].val == udf_name
+            isinstance(target.val, ast.FuncCall) and target.val.funcname[-1] == udf_name
         )
     ]
     # Construct unnest_target_list, which is the target list for the outer query.
     # This consists of unnesting columns that we were previously selecting,
     # as well as unnesting the function call result.
+    outer_target_list_for_where_clause = []
     for target in unnest_target_list:
         val = target.val
+        alias = None
         # The UDF call. Change this to unnest the batched version of the function.
         if isinstance(val, ast.FuncCall):
+            alias = val.funcname[0].val
             val.funcname = [ast.String(val.funcname[0].val + "_batch")]
             new_args = []
             for arg in val.args:
@@ -177,17 +245,26 @@ def transform_query(q, udf_name):
             val.args = new_args
             unnest_call = ast.FuncCall(funcname=[ast.String("unnest")], args=[val])
             target.val = unnest_call
+            target.name = alias
         if isinstance(val, ast.ColumnRef):
+            alias = val.fields[-1].val
             val.fields = [
                 ast.FuncCall(
                     funcname=[ast.String("unnest")],
-                    args=[ast.ColumnRef([ast.String(val.fields[0].val + "_batch")])],
+                    args=[ast.ColumnRef([ast.String(val.fields[-1].val + "_batch")])],
                 )
             ]
+            target.name = alias
         if isinstance(val, ast.A_Const):
             target.val = ast.FuncCall(
                 funcname=[ast.String("unnest")],
                 args=[ast.ColumnRef(["_batch"])],
+            )
+            alias = "unknown"
+            raise Exception("Not implemented")
+        if alias is not None:
+            outer_target_list_for_where_clause.append(
+                ast.ResTarget(val=ast.ColumnRef([alias]))
             )
     subquery.targetList = inner_target_list
     # push down the select statement into a subquery
@@ -196,4 +273,17 @@ def transform_query(q, udf_name):
     )
     select_stmt = ast.SelectStmt(targetList=unnest_target_list, fromClause=(subselect,))
     select_stmt.targetList = unnest_target_list
-    q[0].stmt = select_stmt
+
+    if udf_in_where_clause:
+        outer_select_stmt = ast.SelectStmt(
+            targetList=outer_target_list_for_where_clause,
+            fromClause=(
+                ast.RangeSubselect(
+                    lateral=False, subquery=select_stmt, alias=ast.Alias("dt3")
+                ),
+            ),
+        )
+        outer_select_stmt.whereClause = udf_clause
+        q[0].stmt = outer_select_stmt
+    else:
+        q[0].stmt = select_stmt
