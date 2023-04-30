48a6d988f9dd3edd7ffba3bdd8be39424998de89
kai-franz <kfranz@andrew.cmu.edu>
2023-04-30 13:45:21 -0400

    Rename runner scripts

--- udf/run.py
+++ udf/run.py
@@ -2,10 +2,11 @@
 # this script will rewrite the UDFs to use batched execution and rewrite the queries to use the new UDFs.
 # The rewritten queries and UDFs will be written to the output directory.
 #
-# Usage: python test.py <query_input_dir> <udf_input_dir> <output_dir>
+# Usage: python run.py <query_input_dir> <udf_input_dir> <output_dir>
 
 from rewriter import Rewriter
 import sys
+import time
 
 query_input_dir = sys.argv[1]
 udf_input_dir = sys.argv[2]
@@ -14,6 +15,8 @@ output_dir = sys.argv[3]
 udfs = [1, 5, 6, 7, 12, 13]
 files = [f"sudf_{udf}.sql" for udf in udfs]
 
+rewriting_times = []
+
 
 def rewrite_query(query_file_name, udf_file_name, output_file_name):
     print("Rewriting", query_file_name)
@@ -21,7 +24,11 @@ def rewrite_query(query_file_name, udf_file_name, output_file_name):
         query = query_file.read()
     with open(udf_file_name, "r") as udf_file:
         udf = udf_file.read()
-    r = Rewriter(query, udf, remove_laterals=False)
+    before_time = time.time()
+    for i in range(100):
+        r = Rewriter(query, udf, remove_laterals=True)
+    after_time = time.time()
+    rewriting_times.append(after_time - before_time)
     with open(output_file_name, "w") as out_file:
         out_file.write(r.new_udf())
         out_file.write("\n\n\n")
@@ -32,3 +39,5 @@ for file in files:
     rewrite_query(
         f"{query_input_dir}/{file}", f"{udf_input_dir}/{file}", f"{output_dir}/{file}"
     )
+
+print("Rewriting times:", rewriting_times)
